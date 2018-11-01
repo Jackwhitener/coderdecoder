7f979d2b684fc30033b2613861c16ed57ff1914b
Jackwhitener <jackwhitener1@gmail.com>
2018-11-01 10:25:23 -0400

    Did s'more work, still doesn't quite work, but we're gittin there.

--- coderdecoder.rb
+++ coderdecoder.rb
@@ -1,17 +1,27 @@
 def code(message)
+	message = message.split(//)
 	5.times do
-		message.length.times do |letter|
-			message[letter] = message[letter].succ!
+		message.each do |letter|
+			letter = letter.succ!
+		end
+		message.each do |letter|
+			letter.delete_prefix("a")
 		end
 	end
-	return message
+	return message.join
 end
 def decode(message)
+	message = message.split(//)
 	21.times do
-		message.length.times do |letter|
-			message[letter] = message[letter].succ!
-			puts message
+		message.each do |letter|
+			# puts "active letter: #{letter}"
+			letter = letter.succ!
 		end
+		message.each do |letter|
+			letter = letter.delete_prefix("a")
+			puts letter
+		end
+		puts message
 	end
-	return message
+	return message.join
 end
