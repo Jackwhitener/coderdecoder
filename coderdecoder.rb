ac1b7c5345e2f61ddc57b8eb23abb0f13074d050
Jackwhitener <jackwhitener1@gmail.com>
2018-11-01 09:35:04 -0400

    trying debugging, etc.

--- coderdecoder.rb
+++ coderdecoder.rb
@@ -1,12 +1,6 @@
 def code(message, func)
-	message = message.split(//)
-	codedmessage = ""
 	5.times do
-		message.each do |letter|
-			puts letter
-			letter = letter.succ
-			puts letter
-		end
+		message = message.succ
 	end
 	return message
 end
